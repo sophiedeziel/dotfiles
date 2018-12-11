7f371107bb57711580730dd5ed4960327fc2889e
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-11 14:55:00 -0500

    Make it work for real

--- bash/check_upgrade.sh
+++ bash/check_upgrade.sh
@@ -5,16 +5,20 @@ function _current_epoch() {
 }
 
 function _update_zsh_update() {
-  echo "LAST_EPOCH=$(_current_epoch)" >! .last_update
+  echo "LAST_EPOCH=$(_current_epoch)" >! ~/.dotfiles/.last_update
 }
-if [ -f .zsh-last_update ]; then
-  . .last_update
+
+epoch_target=2
+
+if [ -f  ~/.dotfiles/.last_update ]; then
+  . ~/.dotfiles/.last_update
   if [[ -z "$LAST_EPOCH" ]]; then
     _update_zsh_update
   fi
 
   epoch_diff=$(($(_current_epoch) - $LAST_EPOCH))
   if [ $epoch_diff -gt $epoch_target ]; then
+    cd ~/.dotfiles/ &> /dev/null
     if git fetch origin master --quiet && [ `git rev-list HEAD...origin/master --count` != 0 ]
     then
       echo "[Dotfiles] Would you like to update? [Y/n]: \c"
@@ -25,8 +29,9 @@ if [ -f .zsh-last_update ]; then
       else
       fi
     fi
-
+    cd - &> /dev/null
   fi
+else
+  _update_zsh_update
 fi
-cd - &> /dev/null
 
