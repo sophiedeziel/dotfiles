3ad23e4e3feee677c4f373245ac21535f15d0e68
Sophie Déziel <courrier@sophiedeziel.com>
2019-12-02 15:25:25 -0500

    remove hub references since I'm no longer using it

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,12 +1,9 @@
 alias clean_up_branches='git branch | grep -v "master" | xargs git branch -D'
 alias express='cd ~/dev/express'
 
-alias git=hub
 alias beg="bundle exec guard"
 alias rdm="git diff master --name-only --diff-filter=ACMR | grep '.rb' | ag -v 'schema' | xargs rubocop"
 alias ag='ag --path-to-ignore ~/.agignore'
-
-alias gpr="hub pull-request -i `current_branch_number`"
 
 alias zshconfig="mvim ~/.zshrc"
 alias zshreload="source ~/.zshrc"
