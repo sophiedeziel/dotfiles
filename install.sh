59649ea7b15fd8e0f7b877c4bfc3c30c027f837c
Sophie Déziel <sophie.deziel@shopify.com>
2021-08-05 16:42:51 -0400

    yes all the things

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 if [ -f ~/.oh-my-zsh ]; then 
   echo "Installing Oh-My-Zsh"
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 if ! type "fuck" > /dev/null; then
   echo "Installing The Fuck"
   if type "apt" > /dev/null; then
-    sudo apt update
-    sudo apt install python3-dev python3-pip python3-setuptools
-    sudo pip3 install thefuck
+    yes | sudo apt update
+    yes | sudo apt install python3-dev python3-pip python3-setuptools
+    yes | sudo pip3 install thefuck
   fi
 
   if type "brew" > /dev/null; then
-    brew install thefuck
+    yes | brew install thefuck
   fi
 fi
 
