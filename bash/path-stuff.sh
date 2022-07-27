53e41be9c7d033c1f5a1df1c27f7823eae347894
Sophie Déziel <courrier@sophiedeziel.com>
2022-07-27 11:16:30 -0400

    Add pip's install path to fix thefuck

--- bash/path-stuff.sh
+++ bash/path-stuff.sh
@@ -1,3 +1,4 @@
 export PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export PATH="/usr/local/opt/openssl/bin:$PATH"
 export PATH="/usr/local/heroku/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
